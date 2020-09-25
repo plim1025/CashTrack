77b5b04f182d8739fc1d8efbf3dc14f2902e72e7
plim1025 <plim1025@gmail.com>
2020-09-24 17:24:17 -0700

    Fixed plaid link

--- client/src/components/Accounts/PlaidLinkButton.tsx
+++ client/src/components/Accounts/PlaidLinkButton.tsx
@@ -1,5 +1,5 @@
 // REACT //
-import React, { useState, useCallback, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 
 // COMPONENTS //
 import { usePlaidLink } from 'react-plaid-link';
@@ -8,46 +8,15 @@ import { Button } from 'react-bootstrap';
 // CONTEXT //
 import { ResourcesContext } from '../../App';
 
-const PlaidLinkButton: React.FC = () => {
-    const [accountsInfo, setAccountsInfo] = useState({ token: '', metadata: null });
-    const [accountsLoading, setAccountsLoading] = useState(false);
-    const [linkToken, setLinkToken] = useState(null);
+interface Props {
+    linkToken: string;
+}
+
+const PlaidLinkButton: React.FC<Props> = props => {
     const { refresh } = useContext(ResourcesContext);
 
-    useEffect(() => {
-        const getLinkToken = async () => {
-            try {
-                const response = await fetch(
-                    `${process.env.BACKEND_URI}/api/plaid/create_link_token`,
-                    {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        credentials: 'include',
-                    }
-                );
-                if (!response.ok) {
-                    throw Error('Bad response from server');
-                }
-                const newLinkToken = await response.json();
-                setLinkToken(newLinkToken);
-            } catch (error) {
-                throw Error(`Error fetching link token: ${error}`);
-            }
-        };
-        getLinkToken();
-    }, []);
-
-    const onSuccess = useCallback(async (token: string, metadata: any) => {
-        setAccountsInfo({ token: token, metadata: metadata });
+    const onSuccess = async (token: string, metadata: any) => {
         refresh('Fetching account information...');
-        setAccountsLoading(true);
-    }, []);
-
-    const { open, ready } = usePlaidLink({ token: linkToken, onSuccess: onSuccess });
-
-    const setPlaidAccounts = async (token: string, metadata: any) => {
         try {
             const response = await fetch(`${process.env.BACKEND_URI}/api/plaid/set_account`, {
                 method: 'POST',
@@ -65,19 +34,20 @@ const PlaidLinkButton: React.FC = () => {
             if (!response.ok) {
                 throw Error('Bad response from server');
             }
-        } catch (error) {
-            throw Error(`Error setting plaid account: ${error}`);
+        } catch (err) {
+            throw Error(`Error setting plaid account: ${err}`);
         }
     };
 
-    if (accountsLoading) {
-        const promise = setPlaidAccounts(accountsInfo.token, accountsInfo.metadata).then(() =>
-            setAccountsLoading(false)
-        );
-        throw promise;
-    }
+    const { open, ready, error } = usePlaidLink({ token: props.linkToken, onSuccess: onSuccess });
+
     return (
-        <Button disabled={!ready} onClick={() => open()} size='sm' variant='primary'>
+        <Button
+            disabled={!ready || error !== null}
+            onClick={() => open()}
+            size='sm'
+            variant='primary'
+        >
             Add Account
         </Button>
     );
--- client/src/view/Accounts.tsx
+++ client/src/view/Accounts.tsx
@@ -1,11 +1,42 @@
 // REACT //
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // COMPONENTS //
 import PlaidLinkButton from '../components/Accounts/PlaidLinkButton';
+import FallbackSpinner from '../components/shared/FallbackSpinner';
 
 const Accounts: React.FC = () => {
-    return <PlaidLinkButton />;
+    const [linkToken, setLinkToken] = useState(null);
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+            try {
+                const response = await fetch(
+                    `${process.env.BACKEND_URI}/api/plaid/create_link_token`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        credentials: 'include',
+                    }
+                );
+                if (!response.ok) {
+                    throw Error('Bad response from server');
+                }
+                const newLinkToken = await response.json();
+                setLinkToken(newLinkToken);
+            } catch (error) {
+                throw Error(`Error fetching link token: ${error}`);
+            }
+        };
+        getLinkToken();
+    }, []);
+
+    if (!linkToken) {
+        return <FallbackSpinner backdrop show />;
+    }
+    return <PlaidLinkButton linkToken={linkToken} />;
 };
 
 export default Accounts;
