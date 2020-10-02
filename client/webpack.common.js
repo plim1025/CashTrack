28ce7b59c332b3580081bb30c0793767867341ee
plim1025 <plim1025@gmail.com>
2020-10-02 10:10:08 -0700

    Added relative path to bundle

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -17,7 +17,6 @@ module.exports = {
         filename: '[name].bundle.js',
         chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
     },
     module: {
         rules: [
