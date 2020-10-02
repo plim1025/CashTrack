180f39d59cdc1586de669aee2a39ac4ac515cf39
plim1025 <plim1025@gmail.com>
2020-10-02 01:36:17 -0700

    Added static json

--- src/index.js
+++ src/index.js
@@ -64,7 +64,6 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('SERVED PROPERLY');
     app.use(express.static('client/dist'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
