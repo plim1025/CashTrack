a4920bce1e4c542cf2c063dab06dec28b66a77a8
plim1025 <plim1025@gmail.com>
2020-10-02 01:19:53 -0700

    Added favicon to production build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: './assets/img/favicon.svg' }],
+        }),
+    ],
 });
