9b774c3a466ee58fb9f210fd6aa523a559a3f4d1
plim1025 <plim1025@gmail.com>
2020-10-02 01:45:01 -0700

    Fixed typo with serving dist folder

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.use(middlewares.errorHandler);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/dist'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
     });
 }
 
